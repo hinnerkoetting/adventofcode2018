dd001be0f2d5c5aa8a287ab0d194dd98ae6334ab
Hinnerk Oetting <hinnerk.oetting@gmail.com>
2018-12-02 10:47:42 +0100

    Counting day2 working

--- day2/src/test/groovy/Day2ServiceSpecification.groovy
+++ day2/src/test/groovy/Day2ServiceSpecification.groovy
@@ -25,4 +25,53 @@ class Day2ServiceSpecification extends Specification {
         day2Service.containsLetterTwoTimes()
         day2Service.containsLetterThreeTimes()
     }
+
+    def example3() {
+        given:
+        Day2Service day2Service = new Day2Service();
+
+        when:
+        day2Service.addInputRow('abbcde')
+
+        then:
+        day2Service.containsLetterTwoTimes()
+        !day2Service.containsLetterThreeTimes()
+    }
+
+
+    def example4() {
+        given:
+        Day2Service day2Service = new Day2Service();
+
+        when:
+        day2Service.addInputRow('abcccd')
+
+        then:
+        !day2Service.containsLetterTwoTimes()
+        day2Service.containsLetterThreeTimes()
+    }
+
+    def example5() {
+        given:
+        Day2Service day2Service = new Day2Service();
+
+        when:
+        day2Service.addInputRow('aabcdd')
+
+        then:
+        day2Service.containsLetterTwoTimes()
+        !day2Service.containsLetterThreeTimes()
+    }
+
+    def example6() {
+        given:
+        Day2Service day2Service = new Day2Service();
+
+        when:
+        day2Service.addInputRow('ababab')
+
+        then:
+        !day2Service.containsLetterTwoTimes()
+        day2Service.containsLetterThreeTimes()
+    }
 }
