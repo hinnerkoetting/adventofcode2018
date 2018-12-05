91c1f2c7613854037f7ec32b317ec735240e9691
Hinnerk Oetting <hinnerk.oetting@gmail.com>
2018-12-05 18:40:10 +0100

    Day 3 working

--- day3/src/main/groovy/Day3.groovy
+++ day3/src/main/groovy/Day3.groovy
@@ -5,6 +5,7 @@ new File('src/main/resources/input').each {
     def parts = it.split(':');
     def rect = new Rectangle();
     def leftTop = parts[0].split(',');
+    rect.id = leftTop[0].split('@')[0].trim()
     rect.left = Integer.parseInt(leftTop[0].split('@')[1].trim())
     rect.top = Integer.parseInt(leftTop[1].trim())
     def widthLength = parts[1].split('x')
@@ -18,6 +19,32 @@ new File('src/main/resources/input').each {
     }
 }
 
+
+def singleId;
+new File('src/main/resources/input').each {
+    def parts = it.split(':');
+    def rect = new Rectangle();
+    def leftTop = parts[0].split(',');
+    rect.id = leftTop[0].split('@')[0]
+    rect.left = Integer.parseInt(leftTop[0].split('@')[1].trim())
+    rect.top = Integer.parseInt(leftTop[1].trim())
+    def widthLength = parts[1].split('x')
+    rect.width = Integer.parseInt(widthLength[0].trim())
+    rect.height = Integer.parseInt(widthLength[1].trim())
+
+    boolean candidate = true;
+    for (int x = rect.left; x < rect.left + rect.width; x++) {
+        for (int y = rect.top; y < rect.top + rect.height; y++) {
+            if (square[x][y] != 1){
+                candidate = false;
+            }
+        }
+    }
+    if (candidate) {
+        singleId = rect.id;
+    }
+}
+
 def count = 0;
 for (int x = 0; x < 1000; x++) {
     for (int y = 0; y < 1000; y++) {
@@ -26,8 +53,10 @@ for (int x = 0; x < 1000; x++) {
     }
 }
 printf("Result is $count")
+printf("Single id  $singleId")
 
 class Rectangle {
+    String id;
     int top;
     int left;
     int width;
